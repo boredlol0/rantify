4d86f0cc304ded9304d20f20caa57394d3f93cc5
boredlol0 <74546660+boredlol0@users.noreply.github.com>
2025-06-01 21:26:08 +0530

    Updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,6 @@ export async function middleware(req: NextRequest) {
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  console.log(session)
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-  console.log(user)
 
   // Protect /home route
   if (req.nextUrl.pathname.startsWith('/home')) {
